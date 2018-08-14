28c3842103faeb7e4518ac167fef70073a19e631
Sean <me@sean.taipei>
2018-08-14 19:44:21 +0800

    Aug 14

--- images/generate.sh
+++ images/generate.sh
@@ -2,10 +2,25 @@
 
 mkdir fulls thumbs
 
-for i in {01..20}; do
-	ffmpeg -y -loglevel quiet -i originals/"$i".jpg -vf "scale=1920:-1" fulls/"$i".jpg
-	ffmpeg -y -loglevel quiet -i fulls/"$i".jpg -vf "scale=1920:-1" thumbs/"$i".jpg
-	echo -n .
+for i in {01..40}; do
+	if [[ -f "originals/$i.jpg" ]]; then
+		ffmpeg -y -loglevel quiet -i originals/"$i".jpg -vf "scale=640:-1" thumbs/"$i".jpg
+		if [[ `ls -s thumbs/$i.jpg |cut -d ' ' -f 1` -ge `ls -s originals/$i.jpg |cut -d ' ' -f 1` ]]; then
+			cp originals/"$i".jpg fulls/"$i".jpg
+			cp originals/"$i".jpg thumbs/"$i".jpg
+			echo -n #
+		else
+			ffmpeg -y -loglevel quiet -i originals/"$i".jpg -vf "scale=1920:-1" fulls/"$i".jpg
+			if [[ `ls -s fulls/$i.jpg |cut -d ' ' -f 1` -ge `ls -s originals/$i.jpg |cut -d ' ' -f 1` ]]; then
+				cp originals/"$i".jpg fulls/"$i".jpg
+				echo -n @
+			else
+				echo -n !
+			fi
+		fi
+	else
+		echo -n ' '
+	fi
 done
 echo
 
